f2390eae53eae52929a06e74461dc5692b4dccbd
kuflierl <41301536+kuflierl@users.noreply.github.com>
2025-08-11 14:39:01 +0200

    fix: remove wireless client mode

--- config/kulgo1/sops.nix
+++ config/kulgo1/sops.nix
@@ -5,18 +5,18 @@
     defaultSopsFile = ../../secrets/kulgo1.yaml;
     defaultSopsFormat = "yaml";
     secrets = {
-      "access_points/metered/psk" = {};
-      "access_points/metered/bssid" = {};
-      "access_points/unmetered/psk" = {};
-      "access_points/unmetered/bssid" = {};
-      "wifi/home1/ssid" = {};
-      "wifi/home1/psk" = {};
+      "access_points/metered/psk" = { };
+      "access_points/metered/bssid" = { };
+      "access_points/unmetered/psk" = { };
+      "access_points/unmetered/bssid" = { };
+      #  "wifi/home1/ssid" = {};
+      #  "wifi/home1/psk" = {};
     };
-    templates = {
-      "wifi_env".content = ''
-        HOME1_SSID = "${config.sops.placeholder."wifi/home1/ssid"}"
-        HOME1_PSK = "${config.sops.placeholder."wifi/home1/psk"}"
-      '';
-    };
+    #templates = {
+    #  "wifi_env".content = ''
+    #    HOME1_SSID = "${config.sops.placeholder."wifi/home1/ssid"}"
+    #    HOME1_PSK = "${config.sops.placeholder."wifi/home1/psk"}"
+    #  '';
+    #};
   };
 }
--- config/kulgo1/wireless.nix
+++ config/kulgo1/wireless.nix
@@ -1,11 +1,11 @@
-{ config }:
+{ config, ... }:
 {
   networking.wireless = {
     enable = true;
     interfaces = [
       "wlan0"
     ];
-    environmentFile = config.sops.templates."wifi_env".path;
+    secretsFile = config.sops.templates."wifi_env".path;
     networks = {
       "@HOME1_SSID@" = {
         psk = "@HOME1_PSK@";
