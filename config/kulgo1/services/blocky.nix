49d136ee4d3eabe1596ee71ccca0db252279041d
kuflierl <41301536+kuflierl@users.noreply.github.com>
2025-08-12 02:37:29 +0200

    chore: format blocky config

--- config/kulgo1/services/blocky.nix
+++ config/kulgo1/services/blocky.nix
@@ -10,21 +10,27 @@
       # For initially solving DoH/DoT Requests when no system Resolver is available.
       bootstrapDns = {
         upstream = "https://one.one.one.one/dns-query";
-        ips = [ "1.1.1.1" "1.0.0.1" ];
+        ips = [
+          "1.1.1.1"
+          "1.0.0.1"
+        ];
       };
       #Enable blocking of certain domains.
       blocking = {
         blackLists = {
           #Adblocking
-          ads = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"];
+          ads = [ "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" ];
           #Another filter for blocking adult sites
-          adult = ["https://blocklistproject.github.io/Lists/porn.txt"];
+          adult = [ "https://blocklistproject.github.io/Lists/porn.txt" ];
           #You can add additional categories
         };
         #Configure what block categories are used
         clientGroupsBlock = {
           default = [ "ads" ];
-          children = ["ads" "adult"];
+          children = [
+            "ads"
+            "adult"
+          ];
         };
       };
       caching = {
