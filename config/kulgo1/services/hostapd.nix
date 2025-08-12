8ced7c0310d9c78f7be7698160b78d7b3420533a
kuflierl <41301536+kuflierl@users.noreply.github.com>
2025-08-12 04:57:10 +0200

    fix: hard code channel for radio 2

--- config/kulgo1/services/hostapd.nix
+++ config/kulgo1/services/hostapd.nix
@@ -12,7 +12,7 @@
       "wlp1s0u1u1" = {
         band = "2g";
         countryCode = "DE";
-        channel = 0; # ACS
+        channel = 9; # ACS
         networks = {
           "wlp1s0u1u1" = {
             ssid = "${config.networking.hostName}-2g";
