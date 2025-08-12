b9743cc6f242bfbf3086bda29879a87a9b91a3fd
kuflierl <41301536+kuflierl@users.noreply.github.com>
2025-08-12 20:02:28 +0200

    feat: add user mflierl

--- config/kulgo1/users.nix
+++ config/kulgo1/users.nix
@@ -10,5 +10,11 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGNZCmEzMtQ9q/p8PbyITR3F4FYWdnYj6pNLgwb8k5BV kuflierl@kul6"
       ];
     };
+    users."mflierl" = {
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDw2wn8wz/BNwbCtDu5cbsyVFuCRe/lnsmVRsj1GaRIXoGRAAZ9IhPOkQRDfqdGi0Tu5vx7cByBn3euZnbQMSVcys9dj76sf87vz8vckO0iEF3blzeXjpXTBjJEjDRVyk7xZewqYRsAl7G+QvBISqFlSIZh1u3MoD63dokqatdAOsWJ6LkCNePrx5XTLvHYeh0IA/Oib/PYkXRGd089KMOxxzWU15k6XaF7msDCj7HfFS0liqJJ2PfOeEBQjEyG75zSGzlFqas8XooCG3/VQ+eHB22yH7vkQo47/nuzdGqBK3Fut1IhxXbcS1uKAbswLdOU+KcuCbEQ7DCAL2hLbIH32yvn0xwdpT5kj8rZYDWMdNWUtpyOVG5tgSyvlkrLQJO3CwGggWZZN8RdaEff/glIQRxqMKkJ8Sp2xwPB06PWQUDqJCCpkvdlj96wSIsBaqZGWMIwiSa8+irt5PgalcExvmsxw4peLo7bEkXDFM129rL5UPc1O+TYRNCKTGV+92c= mflierl@msl4.local" 
+      ];
+    };
   };
 }
