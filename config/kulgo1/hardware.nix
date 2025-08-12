42299a0e1811dac749fcf3db62cd59ceeaa1b39e
kuflierl <41301536+kuflierl@users.noreply.github.com>
2025-08-12 02:14:57 +0200

    fix: try to fix by removing potentially different boot stuff

--- config/kulgo1/hardware.nix
+++ config/kulgo1/hardware.nix
@@ -18,18 +18,6 @@
     ];
   };
 
-  hardware = {
-    raspberry-pi."4" = {
-      xhci.enable = true;
-
-      apply-overlays-dtmerge.enable = true;
-    };
-    deviceTree = {
-      enable = true;
-      filter = "*rpi-4-*.dtb";
-    };
-  };
-
   # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
   boot.loader.grub.enable = false;
   # Enables the generation of /boot/extlinux/extlinux.conf
